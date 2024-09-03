0b52466dd253830bc8f4cafd54731b653c4562e6
AloValdes <alo.val.mora@outlook.com>
2024-09-03 12:19:31 -0600

    tercer trabajo,aprendemos la función de column, row y Box

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.proyecto1
 
+import android.inputmethodservice.Keyboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,19 +20,30 @@ import com.example.proyecto1.ui.theme.Proyecto1Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            Proyecto1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            //Layouts
+            Column {
+                Text(text = "First Row")
+                Text(text = "Second Row")
+                Text(text = "Third Row")
+                Text(text = "Fourth Row")
+                Row{Text(text = "HOLA 1")
+                    Text(text = "HOLA 2")
+                    Text(text = "HOLA 3")
+                    Text(text = "HOLA 4")}
+                Box{
+                    Text(text = "Label 1")
+                    Text(text = "Label 2")
+                    Text(text = "Label 3")
                 }
+                Greeting(name: "Hello World")
+                }
+
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -42,6 +57,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Proyecto1Theme {
-        Greeting("Android")
+        Greeting("Alo")
     }
 }
