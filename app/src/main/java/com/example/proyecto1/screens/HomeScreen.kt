b4383a2c9a24a6af36374548e2f48c6c385c3bb8
AloValdes <alo.val.mora@outlook.com>
2024-09-19 07:50:35 -0600

    Proyecto con roblox, arreglar este problema en casa

---
+++ app/src/main/java/com/example/proyecto1/screens/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.proyecto1.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column {
+        Text(text = "This is the Home Screen")
+        Button(onClick = { navController.navigate("Menu") }) {
+            Text(text = "Go to Menu")
+        }
+    }
+}
