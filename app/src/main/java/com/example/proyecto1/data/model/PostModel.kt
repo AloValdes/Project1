45a3cc18b567483051d03b0f84d7c9495d64a7d2
AloValdes <alo.val.mora@outlook.com>
2024-10-08 07:47:57 -0600

    Unidad 2, trabajo 7, vemos sobre los tipos de Post

--- app/src/main/java/com/example/proyecto1/data/model/PostModel.kt
+++ app/src/main/java/com/example/proyecto1/data/model/PostModel.kt
@@ -1,8 +1,11 @@
 package com.example.proyecto1.data.model
 
+import androidx.compose.ui.graphics.painter.Painter
+
 data class PostModel(
-    val id: Int,
-    var title: String,
-    val text: String,
+    val id:Int,
+    var title:String,
+    val text:String,
+    val image:Painter
 
     )
