8fce4e164d4139611af907574d1fed7952d435f6
AloValdes <alo.val.mora@outlook.com>
2024-10-24 18:57:45 -0600

    Unidad 2, trabajo 8, ya jala :(

--- app/src/main/java/com/example/proyecto1/ui/theme/components/PostCard.kt
+++ app/src/main/java/com/example/proyecto1/ui/theme/components/PostCard.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.proyecto1.R
-import com.example.proyecto1.data.model.PostModel
+import com.example.proyecto1.data.Model.PostModel
 
 @Composable
 fun PostCard(id: Int, title: String, text: String, image: Painter){
