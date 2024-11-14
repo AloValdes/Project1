fb222ceb3a893eaa24a413571d1b05a1dcc5c95f
AloValdes <alo.val.mora@outlook.com>
2024-11-14 07:53:05 -0600

    Feat :sparkles: Creamos rama  nueva lllamada "test-app", vemos sobre cómo recibir objetos y mandarlos (get, update, post, delete)

---
+++ app/src/main/java/com/example/proyecto1/data/model/ServiceModel.kt
@@ -0,0 +1,10 @@
+package com.example.proyecto1.data.model
+
+data class ServiceModel(
+    var id: Int = 0,
+    var name: String = "",
+    var usernme: String = "",
+    var password: String = "",
+    var description: String = "",
+    var imageUrl: String? = null
+)
