c8edda3acb5e193f72783512d3eca322f7bd6a33
AloValdes <alo.val.mora@outlook.com>
2024-11-17 15:45:38 -0600

    Feat :sparkles: APIS listas, se movieron todas a una  nueva rama donde estuvieran la mayoría de clases hechas en la materia

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -1,100 +1,73 @@
 package com.example.proyecto1
 
-import android.graphics.Picture
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Icon
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-//import androidx.compose.ui.text.style.TextForegroundStyle.Unspecified.brush
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.example.proyecto1.ui.theme.Proyecto1Theme
 import com.example.proyecto1.ui.theme.screens.HomeScreen
 import com.example.proyecto1.ui.theme.screens.MenuScreen
-import java.security.AccessController
-
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.example.proyecto1.ui.theme.screens.APisScreen
 import com.example.proyecto1.ui.theme.screens.ComponentScreen
-import com.example.proyecto1.ui.theme.screens.HomeScreen
-import com.example.proyecto1.ui.theme.screens.MenuScreen
+import com.example.proyecto1.ui.theme.screens.LoginScreen
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.example.proyecto1.ui.theme.biometrics.BiometricsScreen
+import com.example.proyecto1.ui.theme.location.MapsSearchView
+import com.example.proyecto1.ui.theme.screens.CameraScreen
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+
+
+        // Establecer contenido usando Jetpack Compose
         setContent {
-            multiScreenApp()
+            ProvideWindowInsets {
 
+                ComposeMultiScreenApp(this)
+            }
         }
     }
 }
 
-
 @Composable
-fun multiScreenApp(){
-    val navController=rememberNavController()
-    Surface (color = Color.White){
-        setupNavGraph(navController=navController)
+fun ComposeMultiScreenApp(activity: AppCompatActivity) {
+    val navController = rememberNavController()
+    Surface(color = Color.White) {
+        SetupNavGraph(navController = navController, activity = activity)
     }
 }
 
 @Composable
-fun setupNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "Menu") {
-        composable("Menu") {
-            MenuScreen(navController)
+fun SetupNavGraph(navController: NavHostController, activity: AppCompatActivity) {
+    NavHost(navController = navController, startDestination = "biometrics") {
+        composable("menu") { MenuScreen(navController) }
+        composable("home") { HomeScreen(navController) }
+        composable("components") { ComponentScreen(navController) }
+        composable("biometrics"){ BiometricsScreen(navController = navController, activity = activity) }
+
+        composable("login") { LoginScreen(navController) }
+        composable("apis") { APisScreen(navController) }
+        composable("MapsSearchView/{lat}/{long}/{address}") { backStackEntry ->
+            val lat = backStackEntry.arguments?.getString("lat")?.toDouble() ?: 0.0
+            val long = backStackEntry.arguments?.getString("long")?.toDouble() ?: 0.0  // Convert to Double here
+            val address = backStackEntry.arguments?.getString("address") ?: ""
+            MapsSearchView(lat, long, address)
         }
-        composable("Home") {
-            HomeScreen(navController)
-        }
-        composable("Components") {
-            ComponentScreen(navController)
+        composable("CameraScreen") {
+            CameraScreen(context = LocalContext.current)
+
         }
     }
 }
