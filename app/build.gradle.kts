25ad6bbffc5bd806cc3d21ad9efdc1c2136a4924
AloValdes <alo.val.mora@outlook.com>
2024-11-17 19:49:46 -0600

    Feat :sparkles: Se volvieron a agregar las API´s

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.adaptive.android)
-
-    // implementation(libs.androidx.navigation.runtime.ktx)
+    //implementation(libs.androidx.adaptive.android)
+    //implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -69,5 +69,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
     implementation("io.coil-kt:coil-compose:2.4.0")
 }
---
+++ app/src/main/java/com/example/proyecto1/ui/theme/biometrics/BiometricPromptManager.kt
@@ -0,0 +1,4 @@
+package com.example.proyecto1.ui.theme.biometrics
+
+class BiometricPromptManager {
+}
