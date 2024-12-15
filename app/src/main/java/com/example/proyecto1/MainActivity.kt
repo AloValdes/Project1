4fbdb57844809211b6edd8d093a4ef81a5c22279
AloValdes <alo.val.mora@outlook.com>
2024-12-14 19:24:04 -0600

    Feat :sparkles: ya funciona

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : FragmentActivity() {
         }
     }
 }
-@Preview(showBackground = true)
+
 @Composable
 fun ComposeMultiScreenApp(){
     val navController = rememberNavController()
